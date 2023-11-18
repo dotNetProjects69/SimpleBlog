446d4ed33c3d67835421f4aec84a9848f155be62
Iskandarus <35442045+Iskandarus@users.noreply.github.com>
2023-11-18 19:50:13 +0500

    Bug fix
    Account Controller using SqlExtension class
    Account Index page - changed button type
    New post is button, instead simple link

--- Controllers/PersonalAccountController.cs
+++ Controllers/PersonalAccountController.cs
@@ -2,7 +2,6 @@
 using Microsoft.Data.Sqlite;
 using SimpleBlog.Controllers.Extensions;
 using SimpleBlog.Models.Account;
-using System.Globalization;
 
 namespace SimpleBlog.Controllers
 {
@@ -21,11 +20,13 @@ namespace SimpleBlog.Controllers
         {
             if (Models.TempData.AccountTableName == string.Empty)
                 return RedirectToAction("Index", "SignUp");
-            AccountInfoModel accountModel = InstantiateAccountModel<AccountInfoModel>();
+            AccountInfoModel accountModel = SqlExtensions
+                                            .InstantiateAccountModel<AccountInfoModel>("nickname",
+                                                                                       Models.TempData.AccountTableName);
             return View(accountModel);
         }
 
-        [HttpPost]
+        
         public IActionResult Delete()
         {
             DeleteAccountData();
@@ -34,17 +35,18 @@ namespace SimpleBlog.Controllers
             return RedirectToAction("Index", "SignUp");
         }
 
-        [HttpPost]
+        
         public IActionResult LogOut()
         {
             Models.TempData.AccountTableName = string.Empty;
             return RedirectToAction("Index", "SignUp");
         }
 
-        [HttpPost]
+        
         public IActionResult Update(EditAccountModel model)
         {
-            model = InstantiateAccountModel<EditAccountModel>();
+            model = SqlExtensions.InstantiateAccountModel<EditAccountModel>("NickName",
+                                                                            Models.TempData.AccountTableName);
             return View("EditAccount", model);
         }
 
@@ -57,7 +59,7 @@ namespace SimpleBlog.Controllers
 
         private void DeleteAccountData()
         {
-            string sqlCommand = $"DELETE FROM AuthData WHERE nickname = '{Models.TempData.AccountTableName}'";
+            string sqlCommand = $"DELETE FROM AuthData WHERE NickName = '{Models.TempData.AccountTableName}'";
             SqliteConnection connection = new(_configuration.GetConnectionString("AccountsData"));
             SqliteCommand command = new(sqlCommand, connection);
             connection.Open();
@@ -70,54 +72,6 @@ namespace SimpleBlog.Controllers
                 Console.WriteLine(ex.Message);
             }
         }
-
-        private T InstantiateAccountModel<T>() where T : class, IAccountModel, new()
-        {
-            T model = new();
-            string sqlCommand = $"SELECT * FROM AuthData WHERE nickname = '{Models.TempData.AccountTableName}'";
-            using SqliteConnection connection = new(_configuration.GetConnectionString("AccountsData"));
-            using SqliteCommand command = new(sqlCommand, connection);
-            connection.Open();
-            try
-            {
-                SqliteDataReader reader = command.ExecuteReader();
-                if (!reader.HasRows) return model;
-                reader.Read();
-
-
-                // use Visitor pattern
-                if (model is EditAccountModel)
-                    model = SetData(model as EditAccountModel ?? new(), reader) as T ?? new T();
-                else 
-                    model = SetData(model, reader);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return model;
-        }
-
-        private static T SetData<T>(T model, SqliteDataReader reader) where T : IAccountModel
-        {
-            model.Id = new(reader.GetString(6));
-            model.Name = reader.GetString(1);
-            model.Surname = reader.GetString(2);
-            _ = DateOnly.TryParse(reader.GetString(3), 
-                                  CultureInfo.InvariantCulture, 
-                                  DateTimeStyles.None, 
-                                  out DateOnly dateOnly);
-            model.DateOfBirth = dateOnly;
-            model.Email = reader.GetString(4);
-            model.NickName = reader.GetString(7);
-            return model;
-        }
-
-        private static EditAccountModel SetData(EditAccountModel model, SqliteDataReader reader)
-        {
-            model = SetData<EditAccountModel>(model, reader);
-            model.Password = reader.GetString(5);
-            return model;
-        }
+        
     }
 }
